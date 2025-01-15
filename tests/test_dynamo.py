492201826faec47e472eba993663cdbb43264e19
Jesse Zwaan <j.k.zwaan@gmail.com>
2025-01-15 00:21:35 -0500

    test!: store contract and test suite
    Requires dynamo instance for running tests

---
+++ tests/test_dynamo.py
@@ -0,0 +1,20 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+import uuid
+
+import aioboto3
+
+from brrr.backends.dynamo import DynamoDbMemStore
+from brrr.store import Store
+
+from .store_contract import MemoryContract
+
+
+class TestDynamoByteStore(MemoryContract):
+    @asynccontextmanager
+    async def with_store(self) -> AsyncIterator[Store]:
+        async with aioboto3.Session().client("dynamodb") as dync:
+            table_name = f"brrr_test_{uuid.uuid4().hex}"
+            memory = DynamoDbMemStore(dync, table_name)
+            await memory.create_table()
+            yield memory
