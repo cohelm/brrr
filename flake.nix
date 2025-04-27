1c4913cbc24ec87f738f69bfed373a6825a0e29e
Robin <robin.lewis@anterior.com>
2025-04-27 15:29:51 -0400

    refactor: separate flake.parts module for linux
    Don’t expose a docker package for darwin

--- flake.nix
+++ flake.nix
@@ -39,237 +39,246 @@
     };
   };
 
-  outputs = { self, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = { self, flake-parts, ... }@inputs: let
+    # flake.parts module for linux systems
+    brrrLinux = {
+      perSystem = { config, lib, pkgs, self', ... }: lib.mkIf pkgs.stdenv.isLinux {
+        packages.docker = pkgs.dockerTools.buildLayeredImage {
+          name = "brrr-demo";
+          tag = "latest";
+          config.Entrypoint = [ "${lib.getExe self'.packages.brrr-demo}" ];
+        };
+      };
+    };
+    # flake.parts module for any system
+    brrrAllSystems = {
+      flake = {
+        # A reusable process-compose module (for flake-parts) with either a full
+
+        # demo environment, or just the dependencies if you want to run a server
+        # manually.
+        processComposeModules = {
+          brrr-demo = inputs.services-flake.lib.multiService ./brrr-demo.service.nix;
+          dynamodb = import ./dynamodb.service.nix;
+          localstack = import ./localstack.service.nix;
+           default = { pkgs, ... }: {
+            imports = with self.processComposeModules; [
+              brrr-demo
+              dynamodb
+              # Unused for now but will probably be reintroduced for an SQS demo
+              # soon.
+              localstack
+            ];
+            services = let
+              demoEnv = {
+                AWS_DEFAULT_REGION = "us-east-1";
+                AWS_ENDPOINT_URL = "http://localhost:8000";
+                AWS_ACCESS_KEY_ID = "000000000000";
+                AWS_SECRET_ACCESS_KEY = "fake";
+              };
+            in {
+              redis.r1.enable = true;
+              dynamodb.enable = true;
+              brrr-demo.worker = {
+                package = self.packages.${pkgs.system}.brrr-demo;
+                args = [ "worker" ];
+                environment = demoEnv;
+              };
+              brrr-demo.server = {
+                package = self.packages.${pkgs.system}.brrr-demo;
+                args = [ "server" ];
+                environment = demoEnv;
+              };
+            };
+          };
+        };
+        # WIP, exporting is best effort.
+        nixosModules = {
+          brrr-demo = import ./brrr-demo.module.nix;
+        };
+      };
+      perSystem = { config, self', inputs', pkgs, lib, system, ... }: let
+        python = pkgs.python312;
+        devPackages = [
+          pkgs.process-compose
+          pkgs.redis # For the CLI
+          self'.packages.uv
+        ];
+        brrrpy = pkgs.callPackage ./python/package.nix {
+          inherit (inputs)
+            pyproject-build-systems
+            pyproject-nix
+            uv2nix;
+          inherit python;
+        };
+      in {
+        config = {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            # dynamodb
+            config.allowUnfree = true;
+          };
+          process-compose.demo = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+              self.processComposeModules.default
+            ];
+            cli.options.no-server = true;
+            services.brrr-demo.server.enable = true;
+            services.brrr-demo.worker.enable = true;
+          };
+          process-compose.deps = {
+            imports = [
+              inputs.services-flake.processComposeModules.default
+              self.processComposeModules.default
+            ];
+            cli.options.no-server = true;
+            services.brrr-demo.server.enable = false;
+            services.brrr-demo.worker.enable = false;
+          };
+          packages = {
+            inherit python;
+            inherit (pkgs) uv;
+            inherit (brrrpy) brrr brrr-venv-test;
+            default = brrrpy.brrr-venv;
+            # Stand-alone brrr_demo.py script
+            brrr-demo = pkgs.stdenvNoCC.mkDerivation {
+              name = "brrr-demo.py";
+              dontUnpack = true;
+              installPhase = ''
+                mkdir -p $out/bin
+                cp ${./brrr_demo.py} $out/bin/brrr_demo.py
+              '';
+              buildInputs = [
+                brrrpy.brrr-venv-test
+              ];
+              # The patch phase will automatically use the python from the venv as
+              # the interpreter for the demo script.
+              meta.mainProgram = "brrr_demo.py";
+            };
+          };
+          checks = {
+            pytestIntegration = pkgs.callPackage ./brrr-integration.test.nix { inherit self; };
+            demoNixosTest = pkgs.callPackage ./brrr-demo.test.nix { inherit self; };
+          } // brrrpy.brrr.tests;
+          devshells = {
+            default = {
+              packages = devPackages ++ [
+                self'.packages.python
+              ];
+              motd = ''
+                This is the generic devshell for brrr development.  Use this to fix
+                problems in the Python lockfile and to access generic tooling.
+
+                Available tools:
+              '' + lib.concatLines (map (x: "  - ${x.pname or x.name}") devPackages) + ''
+
+                For Python-specific development, use: nix develop .#python
+              '';
+              env = [
+                {
+                  name = "PYTHONPATH";
+                  unset = true;
+                }
+                {
+                  name = "UV_PYTHON_DOWNLOADS";
+                  value = "never";
+                }
+              ];
+            };
+            python = {
+              env = [
+                {
+                  name = "REPO_ROOT";
+                  eval = "$(git rev-parse --show-toplevel)";
+                }
+                {
+                  name = "PYTHONPATH";
+                  unset = true;
+                }
+                {
+                  name = "UV_PYTHON_DOWNLOADS";
+                  value = "never";
+                }
+                {
+                  name = "UV_NO_SYNC";
+                  value = "1";
+                }
+              ];
+              packages = devPackages ++ [
+                brrrpy.brrr-venv-editable
+              ];
+              commands = [
+                {
+                  name = "brrr-test-unit";
+                  category = "test";
+                  help = "Tests which don't need dependencies";
+                  command = ''
+                    pytest -m 'not dependencies' "$@"
+                  '';
+                }
+                {
+                  name = "brrr-test-all";
+                  category = "test";
+                  help = "Tests including dependencies, make sure to run brrr-demo-deps";
+                  # Lol
+                  command = ''(
+                    : "''${AWS_DEFAULT_REGION=fake}"
+                    export AWS_DEFAULT_REGION
+                    : "''${AWS_ENDPOINT_URL=http://localhost:8000}"
+                    export AWS_ENDPOINT_URL
+                    : "''${AWS_ACCESS_KEY_ID=fake}"
+                    export AWS_ACCESS_KEY_ID
+                    : "''${AWS_SECRET_ACCESS_KEY=fake}"
+                    export AWS_SECRET_ACCESS_KEY
+                    exec pytest "$@"
+                  )'';
+                }
+                # Always build aarch64-linux
+                {
+                  name = "brrr-build-docker";
+                  category = "build";
+                  help = "Build and load a Docker image (requires a Nix Linux builder)";
+                  command = let
+                    drv = self'.packages.docker;
+                  in ''
+                    (
+                      set -o pipefail
+                      if nix build --no-link --print-out-paths .#packages.aarch64-linux.docker | xargs -r docker load -i; then
+                        echo 'Start a new worker with `docker run <image name>`'
+                      fi
+                    )
+                  '';
+                }
+                {
+                  name = "brrr-demo-full";
+                  category = "demo";
+                  help = "Launch a full demo locally";
+                  command = ''
+                    nix run .#demo
+                  '';
+                }
+                {
+                  name = "brrr-demo-deps";
+                  category = "demo";
+                  help = "Start all dependent services without any brrr workers / server";
+                  command = ''
+                    nix run .#deps
+                  '';
+                }
+              ];
+            };
+          };
+        };
+      };
+    };
+  in flake-parts.lib.mkFlake { inherit inputs; } {
     systems = import inputs.systems;
     imports = [
       inputs.process-compose-flake.flakeModule
       inputs.devshell.flakeModule
+      brrrLinux
+      brrrAllSystems
     ];
-    # A reusable process-compose module (for flake-parts) with either a full
-
-    # demo environment, or just the dependencies if you want to run a server
-    # manually.
-    flake = {
-      processComposeModules = {
-        brrr-demo = inputs.services-flake.lib.multiService ./brrr-demo.service.nix;
-        dynamodb = import ./dynamodb.service.nix;
-        localstack = import ./localstack.service.nix;
-         default = { pkgs, ... }: {
-          imports = with self.processComposeModules; [
-            brrr-demo
-            dynamodb
-            # Unused for now but will probably be reintroduced for an SQS demo
-            # soon.
-            localstack
-          ];
-          services = let
-            demoEnv = {
-              AWS_DEFAULT_REGION = "us-east-1";
-              AWS_ENDPOINT_URL = "http://localhost:8000";
-              AWS_ACCESS_KEY_ID = "000000000000";
-              AWS_SECRET_ACCESS_KEY = "fake";
-            };
-          in {
-            redis.r1.enable = true;
-            dynamodb.enable = true;
-            brrr-demo.worker = {
-              package = self.packages.${pkgs.system}.brrr-demo;
-              args = [ "worker" ];
-              environment = demoEnv;
-            };
-            brrr-demo.server = {
-              package = self.packages.${pkgs.system}.brrr-demo;
-              args = [ "server" ];
-              environment = demoEnv;
-            };
-          };
-        };
-      };
-      # WIP, exporting is best effort.
-      nixosModules = {
-        brrr-demo = import ./brrr-demo.module.nix;
-      };
-    };
-    perSystem = { config, self', inputs', pkgs, lib, system, ... }: let
-      python = pkgs.python312;
-      devPackages = [
-        pkgs.process-compose
-        pkgs.redis # For the CLI
-        self'.packages.uv
-      ];
-      brrrpy = pkgs.callPackage ./python/package.nix {
-        inherit (inputs)
-          pyproject-build-systems
-          pyproject-nix
-          uv2nix;
-        inherit python;
-      };
-    in {
-      config = {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          # dynamodb
-          config.allowUnfree = true;
-        };
-        process-compose.demo = {
-          imports = [
-            inputs.services-flake.processComposeModules.default
-            self.processComposeModules.default
-          ];
-          cli.options.no-server = true;
-          services.brrr-demo.server.enable = true;
-          services.brrr-demo.worker.enable = true;
-        };
-        process-compose.deps = {
-          imports = [
-            inputs.services-flake.processComposeModules.default
-            self.processComposeModules.default
-          ];
-          cli.options.no-server = true;
-          services.brrr-demo.server.enable = false;
-          services.brrr-demo.worker.enable = false;
-        };
-        packages = {
-          inherit python;
-          inherit (pkgs) uv;
-          inherit (brrrpy) brrr brrr-venv-test;
-          default = brrrpy.brrr-venv;
-          # Stand-alone brrr_demo.py script
-          brrr-demo = pkgs.stdenvNoCC.mkDerivation {
-            name = "brrr-demo.py";
-            dontUnpack = true;
-            installPhase = ''
-              mkdir -p $out/bin
-              cp ${./brrr_demo.py} $out/bin/brrr_demo.py
-            '';
-            buildInputs = [
-              brrrpy.brrr-venv-test
-            ];
-            # The patch phase will automatically use the python from the venv as
-            # the interpreter for the demo script.
-            meta.mainProgram = "brrr_demo.py";
-          };
-          docker = let
-            pkg = self'.packages.brrr-demo;
-          in pkgs.dockerTools.buildLayeredImage {
-            name = "brrr-demo";
-            tag = "latest";
-            config.Entrypoint = [ "${lib.getExe pkg}" ];
-          };
-        };
-        checks = {
-          pytestIntegration = pkgs.callPackage ./brrr-integration.test.nix { inherit self; };
-          demoNixosTest = pkgs.callPackage ./brrr-demo.test.nix { inherit self; };
-        } // brrrpy.brrr.tests;
-        devshells = {
-          default = {
-            packages = devPackages ++ [
-              self'.packages.python
-            ];
-            motd = ''
-              This is the generic devshell for brrr development.  Use this to fix
-              problems in the Python lockfile and to access generic tooling.
-
-              Available tools:
-            '' + lib.concatLines (map (x: "  - ${x.pname or x.name}") devPackages) + ''
-
-              For Python-specific development, use: nix develop .#python
-            '';
-            env = [
-              {
-                name = "PYTHONPATH";
-                unset = true;
-              }
-              {
-                name = "UV_PYTHON_DOWNLOADS";
-                value = "never";
-              }
-            ];
-          };
-          python = {
-            env = [
-              {
-                name = "REPO_ROOT";
-                eval = "$(git rev-parse --show-toplevel)";
-              }
-              {
-                name = "PYTHONPATH";
-                unset = true;
-              }
-              {
-                name = "UV_PYTHON_DOWNLOADS";
-                value = "never";
-              }
-              {
-                name = "UV_NO_SYNC";
-                value = "1";
-              }
-            ];
-            packages = devPackages ++ [
-              brrrpy.brrr-venv-editable
-            ];
-            commands = [
-              {
-                name = "brrr-test-unit";
-                category = "test";
-                help = "Tests which don't need dependencies";
-                command = ''
-                  pytest -m 'not dependencies' "$@"
-                '';
-              }
-              {
-                name = "brrr-test-all";
-                category = "test";
-                help = "Tests including dependencies, make sure to run brrr-demo-deps";
-                # Lol
-                command = ''(
-                  : "''${AWS_DEFAULT_REGION=fake}"
-                  export AWS_DEFAULT_REGION
-                  : "''${AWS_ENDPOINT_URL=http://localhost:8000}"
-                  export AWS_ENDPOINT_URL
-                  : "''${AWS_ACCESS_KEY_ID=fake}"
-                  export AWS_ACCESS_KEY_ID
-                  : "''${AWS_SECRET_ACCESS_KEY=fake}"
-                  export AWS_SECRET_ACCESS_KEY
-                  exec pytest "$@"
-                )'';
-              }
-              # Always build aarch64-linux
-              {
-                name = "brrr-build-docker";
-                category = "build";
-                help = "Build and load a Docker image (requires a Nix Linux builder)";
-                command = let
-                  drv = self'.packages.docker;
-                in ''
-                  (
-                    set -o pipefail
-                    if nix build --no-link --print-out-paths .#packages.aarch64-linux.docker | xargs -r docker load -i; then
-                      echo 'Start a new worker with `docker run <image name>`'
-                    fi
-                  )
-                '';
-              }
-              {
-                name = "brrr-demo-full";
-                category = "demo";
-                help = "Launch a full demo locally";
-                command = ''
-                  nix run .#demo
-                '';
-              }
-              {
-                name = "brrr-demo-deps";
-                category = "demo";
-                help = "Start all dependent services without any brrr workers / server";
-                command = ''
-                  nix run .#deps
-                '';
-              }
-            ];
-          };
-        };
-      };
-    };
   };
 }
